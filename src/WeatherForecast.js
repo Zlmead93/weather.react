1ba65d0bb3f192dfd7dedc4a8e6f686e6b20fa1a
Zlmead93 <72393696+Zlmead93@users.noreply.github.com>
2021-02-15 16:16:00 +0000

    Update WeatherForecast.js

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -28,5 +28,5 @@ const [forecast,setForecast] = useState (null);
     let apiKey = "5dbe4b73ade41818331f8e929d9c90fe"
     let url = `https://api.openweathermap.org/data/2.5/forecast?q=${props.city}&appid=${apiKey}&units=metric`
 axios.get(url).then(handleForecastResponse);
-    return null; }
+    return props.city; }
 }
